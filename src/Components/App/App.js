61113df06dfe5da5c5cd6253384435e0e677ac76
bhayek12@gmail.com <bhayek12@gmail.com>
2018-10-08 19:37:29 -0600

    Add fetch call to recieve random movie info

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,17 +7,36 @@ import Scroll from '../Scroll/Scroll';
 
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      movieScroll: [],
+      isLoaded: false
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://swapi.co/api/films/")
+      .then(response => response.json())
+      .then(json => this.setState({movieScroll: json.results, isLoaded:true}))
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          SWAPI BOX
-        </header>
-        <Scroll />
-        <Button />
-        <CardContainer />
-      </div>
-    );
+    const { movieScroll, isLoaded } = this.state
+    if(!isLoaded){
+      return <div> Loading.... </div>;
+    } else { 
+      return (
+        <div className="App">
+          <header className="App-header">
+            SWAPI BOX
+          </header>
+          <Scroll movieScroll={ movieScroll }/>
+          <Button />
+          <CardContainer />
+        </div>
+      );
+    }
   }
 }
 
--- src/Components/Scroll/Scroll.js
+++ src/Components/Scroll/Scroll.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './Scroll.css';
 
-const Scroll = (props) => {
+const Scroll = ({ movieScroll }) => {
+  const getRandomIndice = Math.floor(Math.random() * Math.floor(6));
+  const randomMovie = movieScroll[getRandomIndice];
   return (
     <section className='scroll-container'>
+      <p>{randomMovie.opening_crawl}</p>
+      <h3>{randomMovie.title}</h3>
+      <h4>{randomMovie.release_date}</h4>
     </section>
   )
 }
