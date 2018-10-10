0f476353617b1e885bc8d26f595158f2e20c0750
bhayek12@gmail.com <bhayek12@gmail.com>
2018-10-10 15:53:05 -0600

    Test if error is thrown

--- src/Components/API/API.js
+++ src/Components/API/API.js
@@ -2,7 +2,7 @@ export const getMovieScroll = async () => {
   const response = await fetch("https://swapi.co/api/films/")
 
   if(response.status >= 400) {
-    throw(new Error(Error.message))
+    throw new Error('fetch failed')
   } else {
     return await response.json();
   }
--- src/Components/API/API.test.js
+++ src/Components/API/API.test.js
@@ -8,7 +8,16 @@ describe('API', () => {
         json: () => Promise.resolve({results: []})
       }))
     
-
+    const result = await getMovieScroll()
     expect(getMovieScroll()).resolves.toEqual({results: []})
+    })
+
+  it('throw an error if status is not ok', async () => {
+    const expected = Error('fetch failed')
+      window.fetch = jest.fn().mockImplementation(() => ({
+      status: 500,
+        json: () => Promise.resolve({results: []})
+      }))
+    await expect(getMovieScroll()).rejects.toEqual(expected)
     })
   })
