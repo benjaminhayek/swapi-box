603aaacb285ccd01ee4f5f9f598c13f1ef367cdf
Tim Garrity <tag.garrity@gmail.com>
2018-10-10 20:00:37 -0600

    have conditional render check state

--- src/Components/CardContainer/CardContainer.js
+++ src/Components/CardContainer/CardContainer.js
@@ -3,12 +3,15 @@ import './CardContainer.css';
 import Card from '../Card/Card';
 
 const CardContainer = ({starWarsDirectory}) => {
-  console.log(starWarsDirectory)
-  const cards = starWarsDirectory.people.results.map( result => {
-    return <Card 
-              name={result.name}
-            /> 
-  })
+  let cards;
+  debugger;
+  if(typeof starWarsDirectory.people === 'object') {
+    cards = starWarsDirectory.people.results.map( result => {
+       return <Card 
+                 name={result.name}
+               /> 
+     })}
+
   return (
     <section className="card-container">
       {cards}
