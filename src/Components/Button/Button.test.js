eae21d9486a8a2cac036be182465006094858625
bhayek12@gmail.com <bhayek12@gmail.com>
2018-10-11 17:15:31 -0600

    test button funtions

--- src/Components/Button/Button.test.js
+++ src/Components/Button/Button.test.js
@@ -4,13 +4,35 @@ import Button from './Button'
 
 describe('Button', () => {
   let wrapper;
+  let buttonHasBeenPressed = jest.fn()
 
   beforeEach(() => {
-    wrapper = shallow(<Button />);
+    wrapper = shallow(<Button directory={{'people': 'people'}} buttonHasBeenPressed={buttonHasBeenPressed}/>);
   });
 
   it('matches the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should call buttonHasBeenPressed function on click', () => {
+    window.console.log = jest.fn();
+    wrapper.find('.favorites-btn').simulate('click');
+    expect(console.log).toBeCalled();
+  });
+
+  it('should call buttonHasBeenPressed function on click', () => {
+    wrapper.find('.people-btn').simulate('click');
+    expect(buttonHasBeenPressed).toBeCalled();
+  });
+
+  it('should call buttonHasBeenPressed function on click', () => {
+    wrapper.find('.planets-btn').simulate('click');
+    expect(buttonHasBeenPressed).toBeCalled();
+  });
+
+  it('should call buttonHasBeenPressed function on click', () => {
+    wrapper.find('.vehicles-btn').simulate('click');
+    expect(buttonHasBeenPressed).toBeCalled();
   });
 
 });
