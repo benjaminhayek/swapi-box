d3a592515c22fbf25a55220592745bfac8d4aeec
bhayek12@gmail.com <bhayek12@gmail.com>
2018-10-10 18:32:45 -0600

    Reconstruct constructor

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,36 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Button.css';
 
+class Button extends Component => {
+  constructor() {
+    super()
+    this.state = {
 
-const Button = ({searchStarWarsApi, directory}) => {
-  return (
-    <section className="button-container">
-      <button 
-        onClick={console.log('Favorites')} 
-        className='favorites-btn button'>
-        <i className="fab fa-trade-federation"></i>
-          Favorites=0
-      </button>
-      <button 
-        onClick={() => searchStarWarsApi(directory.people)}  
-        className='people-btn button' >
-        <i className="fab fa-jedi-order"></i>
-          People
-      </button>
-      <button 
-        onClick={() => searchStarWarsApi(directory.planets)}  
-        className='planets-btn button'>
-        <i className="fab fa-empire"></i>
-          Planets
-      </button>
-      <button 
-        onClick={() => searchStarWarsApi(directory.vehicles)}  
-        className='vehicles-btn button'>
-        <i className="fab fa-galactic-republic"></i>
-          Vehicles
-      u</button>
-    </section>
-  )
+    }
+  }
+
+  render() {
+    return (
+      <section className="button-container">
+        <button 
+          onClick={console.log('Favorites')} 
+          className='favorites-btn button'>
+          <i className="fab fa-trade-federation"></i>
+            Favorites=0
+        </button>
+        <button 
+          onClick={() => searchStarWarsApi(directory.people)}  
+          className='people-btn button' >
+          <i className="fab fa-jedi-order"></i>
+            People
+        </button>
+        <button 
+          onClick={() => searchStarWarsApi(directory.planets)}  
+          className='planets-btn button'>
+          <i className="fab fa-empire"></i>
+            Planets
+        </button>
+        <button 
+          onClick={() => searchStarWarsApi(directory.vehiclesu)}  
+          className='vehicles-btn button'>
+          <i className="fab fa-galactic-republic"></i>
+            Vehicles
+        </button>
+      </section>
+    )
+  }
 }
 
 export default Button;
