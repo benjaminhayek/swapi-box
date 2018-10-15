95c974f5198d01cbda6688fc638f71643a71d21c
bhayek12@gmail.com <bhayek12@gmail.com>
2018-10-15 17:22:10 -0600

    Add cards to favorites array on button click

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './Card.css';
 
 const Card = (props) => {
+  console.log(props)
   const cardData = props.properties.map((property, i) => {
     return <div key={property + i}>{property}</div>
   })
   return(
-    <div className='card'>
+    <div className='card' id={props.id}>
       <h3>{props.name}</h3>
       {cardData}
       <div>
