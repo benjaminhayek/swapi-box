a7c44c8373115385a83206a1e4590498e13fe9e4
bhayek12@gmail.com <bhayek12@gmail.com>
2018-10-17 17:56:37 -0600

    Add navlink to favs

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import './Button.css';
+import { Route, NavLink } from 'react-router-dom';
 
 const Button = ({ buttonHasBeenPressed, directory, selected }) => {
     return (
       <section className="button-container">
-        <button 
+      <NavLink to='/favorites'
           onClick={() => buttonHasBeenPressed( null,'favorites')} 
           className={`${selected.favorites ? 'selected': null} favorites-btn button`}>
-          <i className="fab fa-trade-federation"></i>
+        <i className="fab fa-trade-federation"></i>
             {`Favorites${directory.favorites.length}`}
-        </button>
-        <button 
+      </NavLink>
+        <NavLink to='/people' 
           onClick={() => buttonHasBeenPressed(directory.people, 'people')}  
           className={`${selected.people ? 'selected': null} people-btn button`} 
         >
           <i className="fab fa-jedi-order"></i>
             People
-        </button>
+        </NavLink>
         <button 
           onClick={() => buttonHasBeenPressed(directory.planets, 'planets')}  
           className={`${selected.planets ? 'selected': null} planets-btn button`} >
