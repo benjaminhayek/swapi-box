655bb6c63bfb89a17d68316093539d93c93e751c
bhayek12@gmail.com <bhayek12@gmail.com>
2018-10-22 07:44:14 -0600

    Lint button file

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -4,43 +4,48 @@ import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const Button = ({ buttonHasBeenPressed, directory, selected }) => {
-    return (
-      <section className="button-container">
-      <NavLink to='/favorites'
-          onClick={() => buttonHasBeenPressed( null,'favorites')} 
-          className={`${selected.favorites ? 'selected': null} favorites-btn button`}>
-        <i className="fab fa-trade-federation"></i>
-            {`Favorites ${directory.favorites.length}`}
+  return (
+    <section className="button-container">
+      <NavLink
+        to="/favorites"
+        onClick={() => buttonHasBeenPressed(null, 'favorites')}
+        className={`${selected.favorites ? 'selected' : null} favorites-btn button`}
+      >
+        <i className="fab fa-trade-federation" />
+        {`Favorites ${directory.favorites.length}`}
       </NavLink>
-        <NavLink to='/people' 
-          onClick={() => buttonHasBeenPressed(directory.people, 'people')}  
-          className={`${selected.people ? 'selected': null} people-btn button`} 
-        >
-          <i className="fab fa-jedi-order"></i>
-            People
-        </NavLink>
-        <NavLink 
-          to='/planets'
-          onClick={() => buttonHasBeenPressed(directory.planets, 'planets')}  
-          className={`${selected.planets ? 'selected': null} planets-btn button`}>
-          <i className="fab fa-empire"></i>
-            Planets
-        </NavLink>
-        <NavLink
-          to='/vehicles'
-          onClick={() => buttonHasBeenPressed(directory.vehicles, 'vehicles')}  
-          className={`${selected.vehicles ? 'selected': null} vehicles-btn button`}>
-          <i className="fab fa-galactic-republic"></i>
-            Vehicles
-        </NavLink>
-      </section>
-    )
-  }
+      <NavLink
+        to="/people"
+        onClick={() => buttonHasBeenPressed(directory.people, 'people')}
+        className={`${selected.people ? 'selected' : null} people-btn button`}
+      >
+        <i className="fab fa-jedi-order" />
+          People
+      </NavLink>
+      <NavLink
+        to="/planets"
+        onClick={() => buttonHasBeenPressed(directory.planets, 'planets')}
+        className={`${selected.planets ? 'selected' : null} planets-btn button`}
+      >
+        <i className="fab fa-empire" />
+          Planets
+      </NavLink>
+      <NavLink
+        to="/vehicles"
+        onClick={() => buttonHasBeenPressed(directory.vehicles, 'vehicles')}
+        className={`${selected.vehicles ? 'selected' : null} vehicles-btn button`}
+      >
+        <i className="fab fa-galactic-republic" />
+          Vehicles
+      </NavLink>
+    </section>
+  );
+};
 
 Button.propTypes = {
   buttonHasBeenPressed: PropTypes.func,
   directory: PropTypes.object,
-  selected: PropTypes.object
+  selected: PropTypes.object,
 };
 
 export default Button;
