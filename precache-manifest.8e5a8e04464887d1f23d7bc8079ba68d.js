df67ac0f97e5afe382879983bf7994d462c1ef17
Tim Garrity <tag.garrity@gmail.com>
2018-10-26 11:11:03 -0600

    Updates

---
+++ precache-manifest.8e5a8e04464887d1f23d7bc8079ba68d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e0d896c1f6567fab1d0da6e2a1ae4e9",
+    "url": "/swapi-box/static/media/mil-falc.6e0d896c.svg"
+  },
+  {
+    "revision": "d9b2065f5cd39b2cce9e31381a10f1c7",
+    "url": "/swapi-box/static/media/death-star.d9b2065f.svg"
+  },
+  {
+    "revision": "05b82421618e7ebdb1144305242bf15c",
+    "url": "/swapi-box/static/media/2048.05b82421.svg"
+  },
+  {
+    "revision": "37aee6c8c1d16c287d66",
+    "url": "/swapi-box/static/js/runtime~main.37aee6c8.js"
+  },
+  {
+    "revision": "476578611f21c50f2b20",
+    "url": "/swapi-box/static/js/main.47657861.chunk.js"
+  },
+  {
+    "revision": "14805db25c97b2f9b642",
+    "url": "/swapi-box/static/js/1.14805db2.chunk.js"
+  },
+  {
+    "revision": "476578611f21c50f2b20",
+    "url": "/swapi-box/static/css/main.b2caa697.chunk.css"
+  },
+  {
+    "revision": "14805db25c97b2f9b642",
+    "url": "/swapi-box/static/css/1.7130cee2.chunk.css"
+  },
+  {
+    "revision": "f007dd17eadc426c5f57e5bebb106a13",
+    "url": "/swapi-box/index.html"
+  }
+];
